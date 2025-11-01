7bc34c5742363e3ad9be88ec7fc5e4f78a6e0fad
Yatul Hamdani <yatulhamdani99@gmail.com>
2025-11-02 04:57:03 +0700

    Add files via upload

---
+++ Add Client PPPoE/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.LblStatus = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Lbl2 = New System.Windows.Forms.Label()
+        Me.Lbl1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(213, 149)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(102, 34)
+        Me.BtnClear.TabIndex = 18
+        Me.BtnClear.Text = "Bersihkan"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Location = New System.Drawing.Point(110, 149)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(102, 34)
+        Me.BtnAdd.TabIndex = 17
+        Me.BtnAdd.Text = "Tambah"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(110, 112)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(205, 20)
+        Me.TxtPassword.TabIndex = 16
+        '
+        'TxtName
+        '
+        Me.TxtName.Location = New System.Drawing.Point(110, 78)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.Size = New System.Drawing.Size(205, 20)
+        Me.TxtName.TabIndex = 15
+        '
+        'LblStatus
+        '
+        Me.LblStatus.AutoSize = True
+        Me.LblStatus.Location = New System.Drawing.Point(130, 228)
+        Me.LblStatus.Name = "LblStatus"
+        Me.LblStatus.Size = New System.Drawing.Size(46, 13)
+        Me.LblStatus.TabIndex = 14
+        Me.LblStatus.Text = "Status : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DodgerBlue
+        Me.Label4.Location = New System.Drawing.Point(68, 23)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(221, 30)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Tambah PPPoE Client"
+        '
+        'Lbl2
+        '
+        Me.Lbl2.AutoSize = True
+        Me.Lbl2.Location = New System.Drawing.Point(19, 119)
+        Me.Lbl2.Name = "Lbl2"
+        Me.Lbl2.Size = New System.Drawing.Size(59, 13)
+        Me.Lbl2.TabIndex = 12
+        Me.Lbl2.Text = "Password :"
+        '
+        'Lbl1
+        '
+        Me.Lbl1.AutoSize = True
+        Me.Lbl1.Location = New System.Drawing.Point(19, 85)
+        Me.Lbl1.Name = "Lbl1"
+        Me.Lbl1.Size = New System.Drawing.Size(41, 13)
+        Me.Lbl1.TabIndex = 11
+        Me.Lbl1.Text = "Name :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 276)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TxtName)
+        Me.Controls.Add(Me.LblStatus)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Lbl2)
+        Me.Controls.Add(Me.Lbl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents LblStatus As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Lbl2 As Label
+    Friend WithEvents Lbl1 As Label
+End Class
---
+++ Add Client PPPoE/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports tik4net
+Public Class Form1
+    Private Connection As ITikConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            Connection = ConnectionFactory.CreateConnection(TikConnectionType.Api)
+            Connection.Open("192.168.200.1", "user1", "user1234")
+        Catch ex As Exception
+            MessageBox.Show("Error connecting to MikroTik: " & ex.Message)
+        End Try
+        LblStatus.Text = "Status : Terhubung ke Mikrotik"
+        LblStatus.ForeColor = Color.Green
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        If TxtName.Text = "" Or TxtPassword.Text = "" Then
+            MessageBox.Show("Nama dan Password tidak boleh kosong", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Try
+            Dim addCommand As ITikCommand = Connection.CreateCommand("/ppp/secret/add")
+            addCommand.AddParameter("name", TxtName.Text)
+            addCommand.AddParameter("password", TxtPassword.Text)
+            addCommand.AddParameter("service", "pppoe")
+            addCommand.ExecuteNonQuery()
+            MessageBox.Show("Client PPPoE berhasil ditambahkan", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TxtName.Clear()
+            TxtPassword.Clear()
+            TxtName.Focus()
+        Catch ex As Exception
+            MessageBox.Show("Error adding PPPoE client: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        If TxtName.Text <> "" Then
+            TxtName.Clear()
+        End If
+        If TxtPassword.Text <> "" Then
+            TxtPassword.Clear()
+        End If
+    End Sub
+End Class
